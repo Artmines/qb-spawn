4beabd9443573b8c723ccd863f17c446fbb03dbd
Artmines <96462463+Artmines@users.noreply.github.com>
2023-03-30 18:35:40 -0600

    NYC coordinates / removed apartments

---
+++ config.lua
@@ -0,0 +1,39 @@
+QB = {}
+
+QB.Spawns = {
+    ["legion"] = {
+        coords = vector4(2.82, 1235.03, 7.17, 176.4),
+        location = "legion",
+        label = "Central Park",
+    },
+
+    ["policedp"] = {
+        coords = vector4(-24.6, -90.62, 4.79, 219.29),
+        location = "policedp",
+        label = "Police Department",
+    },
+
+    ["paleto"] = {
+        coords = vector4(1300.09, 421.56, 36.25, 258.91),
+        location = "paleto",
+        label = "Brooklyn",
+    },
+
+    ["motel"] = {
+        coords = vector4(879.45, 2278.09, 38.49, 82.13),
+        location = "motel",
+        label = "Bronx",
+    },
+
+    ["leftwood"] = {
+        coords = vector4(-907.18, 1772.92, 24.45, 214.81),
+        location = "leftwood",
+        label = "Left Wood",
+    },
+
+    ["newjersey"] = {
+        coords = vector4(-1268.44, 927.25, 19.56, 224.51),
+        location = "newjersey",
+        label = "New Jersey",
+    },
+}
